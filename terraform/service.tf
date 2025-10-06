fe4b3b6403ce0da60cbbe4772d878f3a86aa7d19
Carlos Magno <ferreiracarlosma@gmail.com>
2025-10-06 00:43:41 -0300

    deploy two tasks in ecs

--- terraform/service.tf
+++ terraform/service.tf
@@ -11,5 +11,20 @@ resource "aws_ecs_service" "my_first_services" {
     assign_public_ip = true
   }
   enable_ecs_managed_tags = true
-
 }
+
+resource "aws_ecs_service" "my_game_services" {
+  name                = "service-game-2048"
+  cluster             = aws_ecs_cluster.my_cluster.id
+  task_definition     = aws_ecs_task_definition.game_task.arn
+  launch_type         = "FARGATE"
+  scheduling_strategy = "REPLICA"
+  desired_count       = 1
+
+  network_configuration {
+    subnets          = [aws_default_subnet.ecs_az1.id]
+    assign_public_ip = true
+  }
+  enable_ecs_managed_tags = true
+}
+
--- terraform/taskdefination.tf
+++ terraform/taskdefination.tf
@@ -1,6 +1,16 @@
 resource "aws_ecs_task_definition" "my_php_task" {
-  family                   = "my-php-task"
-  container_definitions    = file("task-definitions/task.json")
+  family                   = "php-task"
+  container_definitions    = file("task-definitions/php.json")
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  cpu                      = 256
+}
+
+resource "aws_ecs_task_definition" "game_task" {
+  family                   = "game-2048-task"
+  container_definitions    = file("task-definitions/game.json")
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   memory                   = 512
